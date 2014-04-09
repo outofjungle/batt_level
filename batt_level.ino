08fc8490698e30a4f054254ca79da94fb4006c7e
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2014-04-08 22:19:58 -0700

    refactored debug msg

--- batt_level.ino
+++ batt_level.ino
@@ -1,16 +1,19 @@
+#include <LiquidCrystal.h>
 #define R1 20000.0
 #define R2 10000.0
 
 float Vin, Vout;
 float weight;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 void setup() {
   Serial.begin(9600);
   weight = (R1 + R2)/(R2);
+  lcd.print("hello, world!");
 }
 
 void loop() {
   Vout = analogRead(A1) * (5.0/1023.0);
-  Serial.println( Vin );
   Vin = weight * Vout;
 
   Serial.print("Vin: ");
